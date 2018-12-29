4d84839298cb17c1cfb4a356cfdfc01af3f7f2f1
ivolgin0349 <ivolgin0349@gmail.com>
2018-12-29 13:13:39 +0300

    Добавьте файлы проекта.

---
+++ WriteArrayToFile/Form1.Designer.cs
@@ -0,0 +1,98 @@
+﻿namespace WriteArrayToFile
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btnSave = new System.Windows.Forms.Button();
+            this.btnClose = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btnSave
+            // 
+            this.btnSave.Location = new System.Drawing.Point(143, 175);
+            this.btnSave.Name = "btnSave";
+            this.btnSave.Size = new System.Drawing.Size(75, 23);
+            this.btnSave.TabIndex = 0;
+            this.btnSave.Text = "Сохранить";
+            this.btnSave.UseVisualStyleBackColor = true;
+            this.btnSave.Click += new System.EventHandler(this.BtnSave_Click);
+            // 
+            // btnClose
+            // 
+            this.btnClose.Location = new System.Drawing.Point(242, 175);
+            this.btnClose.Name = "btnClose";
+            this.btnClose.Size = new System.Drawing.Size(75, 23);
+            this.btnClose.TabIndex = 1;
+            this.btnClose.Text = "Отмена";
+            this.btnClose.UseVisualStyleBackColor = true;
+            this.btnClose.Click += new System.EventHandler(this.BtnClose_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(68, 81);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(90, 18);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Значение 1:";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(160, 80);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(327, 209);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.btnClose);
+            this.Controls.Add(this.btnSave);
+            this.Name = "Form1";
+            this.Text = "Сохранение данных";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btnSave;
+        private System.Windows.Forms.Button btnClose;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ WriteArrayToFile/Form1.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.IO;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace WriteArrayToFile
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        // сохраняем данные в файл
+        private void BtnSave_Click(object sender, EventArgs e)
+        {
+
+            string fileTest = @" C:\Users\i.geraskin\source\repos\CalcData.csv";
+
+
+
+            //Объявляем приложение
+            Excel.Application ex = new Microsoft.Office.Interop.Excel.Application
+            {
+                //Отобразить Excel
+                Visible = true,
+
+                //Количество листов в рабочей книге
+                SheetsInNewWorkbook = 2
+            };
+
+            //Добавить рабочую книгу
+            Excel.Workbook workBook = ex.Workbooks.Add(Type.Missing);
+
+            //Отключить отображение окон с сообщениями
+            ex.DisplayAlerts = false;
+
+            //Получаем первый лист документа (счет начинается с 1)
+            Excel.Worksheet sheet = (Excel.Worksheet)ex.Worksheets.get_Item(1);
+
+            //Название листа (вкладки снизу)
+            sheet.Name = "Данные";
+
+            //Пример заполнения ячеек
+            for (int i = 1; i <= 10; i++)
+            {
+                sheet.Cells[i, 1] = String.Format("Value {0}", i);
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                sheet.Cells[i, 2] = String.Format("Value {0}", i);
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                sheet.Cells[i, 3] = String.Format("Value {0}", i);
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                sheet.Cells[i, 4] = String.Format("Value {0}", i);
+            }
+
+            for (int i = 1; i <= 10; i++)
+            {
+                sheet.Cells[i, 5] = String.Format("Value {0}", i);
+            }
+
+            // добавляем пустую строку
+            Excel.Range cellRange = (Excel.Range)sheet.Cells[1, 1];
+            Excel.Range rowRange = cellRange.EntireRow;
+            rowRange.Insert(Excel.XlInsertShiftDirection.xlShiftDown, false);
+
+            // создаем заголовки
+            sheet.Cells[1, 1] = String.Format("Depth");
+            sheet.Cells[1, 2] = String.Format("Paraffins");
+            sheet.Cells[1, 3] = String.Format("Nom. debit");
+            sheet.Cells[1, 4] = String.Format("Temp_oil");
+            sheet.Cells[1, 5] = String.Format("Temp_wire");
+
+            MessageBox.Show("Данные сохранены", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+
+            sheet.SaveAs(fileTest);
+
+
+            //Excel.Application oApp;
+            //Excel.Application oSheet;
+            //Excel.Application oBook;
+
+            //oApp = new Excel.Application();
+            //oBook = oApp.Workbooks.Add();
+            //oSheet = (Excel.Worksheet)oBook.Worksheets.get_Item(1);
+            //oSheet.Cell[1, 1] = "some value";
+
+            //using (StreamWriter streamWriter = new StreamWriter(fileTest))
+            //{
+
+            //}
+
+
+            //sheet.SaveAs(fileTest);
+            //oBook.Close();
+            //oApp.Quit();
+
+            //SaveFileDialog savefile = new SaveFileDialog
+            //{
+            //    // задаем имя файла по умолчанию
+            //    FileName = "CalcData.csv",
+
+
+            //    // фильтруем по типу
+            //    Filter = "Excel файл CSV|*.csv|All files (*.*)|*.*"
+            //};
+
+            //// открываем диалоговое окно
+            //if (savefile.ShowDialog() == DialogResult.OK)
+            //{
+            //    using (StreamWriter streamWriter = new StreamWriter(savefile.FileName))
+            //    {                    
+
+            //        using (var csvWriter = new CsvWriter(streamWriter))
+            //        {
+            //            // указываем разделитель (каждый заголовок запишется в свою ячейку)
+            //            csvWriter.Configuration.Delimiter = ";";
+
+            //            // записываем заголовки
+            //            csvWriter.WriteField("Depth");
+            //            csvWriter.WriteField("Paraffins");
+            //            csvWriter.WriteField("Nom. debit");
+            //            csvWriter.WriteField("Temp_oil");
+            //            csvWriter.WriteField("Temp_wire");
+            //            csvWriter.NextRecord();                          
+
+            //        }
+            //    }
+
+
+
+            //    //    int.TryParse(textBox1.Text, out int TextBox1);
+            //    //    for (int i = 0; i < TextBox1; i++)
+            //    //    {
+            //    //        // записываем данные в файл
+            //    //        sw.WriteLine(i);                
+
+            MessageBox.Show("Данные успешно сохранены", "Сообщение", MessageBoxButtons.OK, MessageBoxIcon.Information);
+        }
+
+        
+
+        // закрываем окно
+        private void BtnClose_Click(object sender, EventArgs e)
+        {
+            Close();
+        }
+    }
+}
